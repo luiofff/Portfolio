a94936ed71d2478993597f3aa48612f8ffba37f9
luiofff <95875204+luiofff@users.noreply.github.com>
2023-11-18 15:26:29 +0300

    make about

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Main from './Pages/Main/Main';
 
 function App() {
   return (
     <div className="App">
-      
+      <Main />
     </div>
   );
 }
--- client/src/Pages/Main/Main.jsx
+++ client/src/Pages/Main/Main.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import styles from "./Main.module.css"
+
+import About from '../About/About'
+import Projects from "../Projects/Projects"
 
 export default function Main() {
   return (
     <>
-      
+        <div className={styles.header_title}>
+            <h1>ILYA ULANOV</h1>
+        </div>
+
+
+
+        <About />
+        <Projects />
     </>
   )
 }
