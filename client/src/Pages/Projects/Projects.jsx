5a66d918a3640b9c9a9d29fa8c5880bbfeebc47c
luiofff <95875204+luiofff@users.noreply.github.com>
2023-11-18 16:23:34 +0300

    add projects header

--- client/src/Pages/Projects/Projects.jsx
+++ client/src/Pages/Projects/Projects.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
+import styles from './Projects.module.css'
+
 export default function Projects() {
   return (
-    <div>
-      
-    </div>
+    <>
+        <section className={styles.project_section}>
+          <div className={styles.projects_header}>
+            <span className={styles.projects_header_title}>Мои проекты</span>
+          </div>
+        </section>
+    </>
   )
 }
