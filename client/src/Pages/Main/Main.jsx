dbc9e1aa8a5cf7f431990f4b1d181534c1504247
luiofff <95875204+luiofff@users.noreply.github.com>
2023-11-19 01:50:25 +0300

    make scroll logic

--- client/src/Pages/Main/Main.jsx
+++ client/src/Pages/Main/Main.jsx
@@ -1,20 +1,46 @@
-import React from 'react'
-import styles from "./Main.module.css"
-
-import About from '../About/About'
-import Projects from "../Projects/Projects"
+import React, { useState, useEffect } from 'react';
+import { Element } from 'react-scroll';
+import styles from './Main.module.css';
+import About from '../About/About';
+import Projects from '../Projects/Projects';
 
 export default function Main() {
+  const [aboutVisible, setAboutVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+
+      // Adjust the scroll threshold as needed
+      const scrollThreshold = 1450;
+
+      if (scrollPosition > scrollThreshold) {
+        setAboutVisible(false);
+      } else {
+        setAboutVisible(true);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
-        <div className={styles.header_title}>
-            <h1>ILYA ULANOV</h1>
-        </div>
+      <div className={styles.header_title}>
+        <h1>ILYA ULANOV</h1>
+      </div>
 
+      <Element name="about" id="about" className={aboutVisible ? "" : styles.invisible}>
+        <About company />
+      </Element>
 
-
-        <About />
+      <Element name="projects" id="projects">
         <Projects />
+      </Element>
     </>
-  )
+  );
 }
